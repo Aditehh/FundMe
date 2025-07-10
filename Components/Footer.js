dfc2626f4804b5f5661c45e0d9918d0e85318a9a
aditehh <aditya.aryal888@gmail.com>
2025-07-10 18:16:36 +0545

    secondary hassle

---
+++ Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      i am foot
+    </div>
+  )
+}
+
+export default Footer
---
+++ Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      i head 
+    </div>
+  )
+}
+
+export default Navbar
