5f931606f9c719a4957c3517968d4ad35d7e4910
aditehh <aditya.aryal888@gmail.com>
2025-07-10 18:51:57 +0545

    thenksu

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>
-      i am foot
-    </div>
-  )
+    return (
+        <footer className='flex justify-between bg-gray-700 text-white h-12 px-5  items-center'>
+
+            <div className="logo font-bold cursor-pointer">FundMe-2025</div>
+            <ul className='flex gap-5'>
+                <li className='hover:text-gray-300 cursor-pointer hover:underline'>Twitter</li>
+               <a href="https://github.com/Aditehh/FundMe" target='_blank'><li className='hover:text-gray-300 cursor-pointer hover:underline'>Github</li></a> 
+            </ul>
+
+        </footer>
+    )
 }
 
 export default Footer
--- Components/Navbar.js
+++ Components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
     return (
         <nav className='flex justify-around bg-gray-700 text-white h-20  items-center'>
-            <div className="logo text-3xl font-bold cursor-pointer">FundMe</div>
+            <div className="logo text-2xl font-bold cursor-pointer">FundMe</div>
             <ul className='flex gap-5'>
                 <li className='hover:text-gray-300 cursor-pointer'>Home</li>
                 <li className='hover:text-gray-300 cursor-pointer'>About</li>
