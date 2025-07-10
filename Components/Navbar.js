f7240b442b1c6ba525d62649675baa8dece492f9
aditehh <aditya.aryal888@gmail.com>
2025-07-10 21:41:04 +0545

    ui

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className='flex justify-around bg-[#393E46] text-white h-16  items-center'>
-            <div className="logo text-3xl text-[#DFD0B8] font-bold cursor-pointer">FundMe</div>
-            <ul className='flex gap-5'>
-                <li className='text-[#DFD0B8] text-lg cursor-pointer'>Home</li>
-                <li className='text-[#DFD0B8] text-lg cursor-pointer'>About</li>
-                <li className='text-[#DFD0B8] text-lg cursor-pointer'>Contact</li>
+        <nav className='flex  bg-[#393E46] text-white h-16  items-center justify-between p-5'>
+            <div className="logo text-xl md:text-3xl md:p-8 text-[#DFD0B8] font-bold cursor-pointer">JavaJuice</div>
+            <ul className='flex gap-2 p-2 md:p-8 md:gap-5'>
+                <li className='text-[#DFD0B8] text-sm md:text-lg  cursor-pointer'>Home</li>
+                <li className='text-[#DFD0B8] text-sm md:text-lg cursor-pointer'>About</li>
+                <li className='text-[#DFD0B8] text-sm md:text-lg cursor-pointer'>Contact</li>
             </ul>
         </nav>
     )
--- app/page.js
+++ app/page.js
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <>
       <div className="flex gap-3 justify-center flex-col items-center text-[#DFD0B8] h-[91.3vh]">
-        <div className="font-bold text-5xl">Fund your favourite creators</div>
+        <div className="font-bold text-5xl">Fund the devs</div>
         <div className="text-xl">
-          A crowdfunding platform for creators. Get funded by your fans and followers. Start Now!!!
+          A crowdfunding platform for developers. Get funded by your fans and followers. Start Now!!!
         </div>
         <div>
           <button type="button" class=" text-gray-900 bg-gradient-to-r from-red-200 to-yellow-100 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-yellow-400 font-medium rounded-lg text-lg px-5 py-2.5 text-center me-2 mb-2">Start Now</button>
@@ -15,11 +15,24 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="bg-[#948979] h-[70vh]">
+      {/* first section division here */}
 
 
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-          
+      <div className="bg-[#948979] h-[calc(100vh-12vh)] flex items-center justify-center p-10 ">
+        <div class=" bg-white flex flex-col rounded-3xl  text-[#dfd0b8] dark:bg-gray-800 dark:border-gray-700 h-[85vh] w-[82vw] p-3 gap-5  md:items-center md:p-7">
+          <div className="text-lg text-[#dfd0b85a] font-semibold">
+            Support
+          </div>
+          <div className="text-4xl font-semibold text-[#dfd0b8]">
+
+            Give your audience
+            an easy way to say thanks.
+          </div>
+
+          <div className="text-sm text-[#dfd0b8b4]">
+            Fuel your code with JavaJuice.
+            Let your fans support you with a shot of digital caffeine quick payments, kind messages, and zero friction.
+          </div>
         </div>
 
       </div>
