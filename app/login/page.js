ccb7c344f950968bc83c4de9d7c5de6f6c7a9b1f
aditehh <aditya.aryal888@gmail.com>
2025-07-11 21:26:18 +0545

    nextauth

--- app/login/page.js
+++ app/login/page.js
@@ -11,15 +11,15 @@ const Login = () => {
             <div className="flex flex-col gap-2 items-center min-h-screen ">
 
                 <div class="w-full max-w-xs">
-                    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <form className=" shadow-md rounded px-8 pt-6 pb-8 mb-4">
                         <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                            <label className="block text-[#DFD0B8] text-sm font-bold mb-2" for="username">
                                 Username
                             </label>
                             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
                         </div>
                         <div className="mb-6">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+                            <label className="block text-[#DFD0B8] text-sm font-bold mb-2" for="password">
                                 Password
                             </label>
                             <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
