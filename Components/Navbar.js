ec21a8837a7ed5d662a74b02facd11ad1b962865
aditehh <aditya.aryal888@gmail.com>
2025-07-13 12:26:44 +0545

    thsthebestmucjc

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -18,8 +18,8 @@ const Navbar = () => {
         <li className='text-[#DFD0B8] text-sm md:text-lg cursor-pointer '>
 
           {session && (
-            <div className="relative inline-block text-left">
-              <Link href="/dashboard">
+            <div className="relative inline-block text-left px-2 py-1">
+              <Link href="/dashboard"> 
                 {/* You can even wrap the button with Link if needed */}
               </Link>
 
@@ -27,7 +27,7 @@ const Navbar = () => {
                 id="dropdownHoverButton"
                 data-dropdown-toggle="dropdownHover"
                 data-dropdown-trigger="hover"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="text-gray-900 bg-gradient-to-r from-red-200 to-yellow-100 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-yellow-100 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 "
                 type="button"
               >
                 Dropdown hover
