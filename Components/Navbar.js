af210f1f13b7566dc124d33f6c6f3e2ba087ffe2
aditehh <aditya.aryal888@gmail.com>
2025-07-13 14:09:18 +0545

    this

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -6,7 +6,7 @@ import Link from 'next/link'
 
 const Navbar = () => {
   const { data: session } = useSession()
- 
+
 
 
   return (
@@ -19,18 +19,18 @@ const Navbar = () => {
 
           {session && (
             <div className="relative inline-block text-left px-2 py-1">
-              <Link href="/dashboard"> 
+              <Link href="/dashboard">
                 {/* You can even wrap the button with Link if needed */}
               </Link>
 
               <button
                 id="dropdownHoverButton"
-                data-dropdown-toggle="dropdownHover"
-                data-dropdown-trigger="hover"
-                className="text-gray-900 bg-gradient-to-r from-red-200 to-yellow-100 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-yellow-100 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 "
+                // data-dropdown-toggle="dropdownHover"
+                // data-dropdown-trigger="hover"
+                className={`text-gray-900 inline-flex bg-gradient-to-r from-red-200 to-yellow-100 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-yellow-100 font-medium rounded-lg text-sm px-5 py-2 text-center items-center dark:bg-blue-600 dark:hover:bg-blue-700`}
                 type="button"
               >
-                Dropdown hover
+                Hello, {session.user.email}!
                 <svg
                   className="w-2.5 h-2.5 ms-3"
                   aria-hidden="true"
@@ -49,8 +49,8 @@ const Navbar = () => {
               </button>
 
               <div
-                id="dropdownHover"
-                className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700"
+                // id="dropdownHover"
+                className="z-10 hidden group-hover:block bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700"
               >
                 <ul
                   className="py-2 text-sm text-gray-700 dark:text-gray-200"
@@ -81,7 +81,7 @@ const Navbar = () => {
                     </a>
                   </li>
                   <li>
-                    <a
+                    <a onclick={() => signOut()}
                       href="#"
                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
@@ -93,9 +93,9 @@ const Navbar = () => {
             </div>
           )}
 
-
-          <Link href={"/login"}>
-            <button type="button" class=" text-gray-900 bg-gradient-to-r from-red-200 to-yellow-100 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-yellow-100 font-medium rounded-lg text-sm md:text-lg px-2.5 py-1 text-center me-2 mb-2">Login</button> </Link>
+          {!session &&
+            <Link href={"/login"}>
+              <button type="button" class=" text-gray-900 bg-gradient-to-r from-red-200 to-yellow-100 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-yellow-100 font-medium rounded-lg text-sm md:text-lg px-2.5 py-1 text-center me-2 mb-2">Login</button> </Link>}
         </li>
       </ul>
     </nav>
