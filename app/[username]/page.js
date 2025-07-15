3ba255b08a0ceccd0c4478deeaa4ae8924caee6d
aditehh <aditya.aryal888@gmail.com>
2025-07-15 19:32:38 +0545

    kdgj

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -8,6 +8,9 @@ const Username = ({ params }) => {
 
         <div className='cover'>
           <img className='relative' src="https://c10.patreonusercontent.com/4/patreon-media/p/campaign/319574/3a48afb63ec44896b29f632cbe0eec7c/eyJ3IjoxNjAwLCJ3ZSI6MX0%3D/5.jpg?token-hash=TxpINT7GexR_tz9VvM3Bh-pMPlHq1XaF47lZwjXskwc%3D&token-time=1754265600" alt="" />
+          <div>
+
+          </div>
         </div>
 
       </div>
