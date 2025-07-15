a6fdf64543f645f948cfc51dbc9d1f07fbb0bc59
aditehh <aditya.aryal888@gmail.com>
2025-07-15 22:41:22 +0545

    haolp

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -5,13 +5,13 @@ const Username = ({ params }) => {
     <>
       <div className='cover w-full text-white' >
         <img className='relative object-cover w-full ' src="https://c10.patreonusercontent.com/4/patreon-media/p/campaign/319574/3a48afb63ec44896b29f632cbe0eec7c/eyJ3IjoxNjAwLCJ3ZSI6MX0%3D/5.jpg?token-hash=TxpINT7GexR_tz9VvM3Bh-pMPlHq1XaF47lZwjXskwc%3D&token-time=1754265600" alt="" />
-        <div className='absolute right-[48%] top-96 rounded-full' >
+        <div className='absolute right-[45%] top-96 rounded-full' >
           <img className='border-white border-2 h-[110px] w-[150px] rounded-full ' src="https://cdn-useast1.kapwing.com/static/templates/crying-cat-meme-template-full-719a53dc.webp" alt="" />
         </div>
       </div>
 
-      <div className=''>
-        {params.username}
+      <div className='text-white my-16 flex justify-center items-center'>
+        @{params.username}
       </div>
     </>
   )
