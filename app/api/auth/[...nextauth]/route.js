41e45496a60efbaa38ec88ea816de71f5b8505fc
aditehh <aditya.aryal888@gmail.com>
2025-07-18 19:13:48 +0545

    this

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,60 +1,64 @@
 import NextAuth from 'next-auth'
+// import AppleProvider from 'next-auth/providers/apple'
+// import FacebookProvider from 'next-auth/providers/facebook'
+// import GoogleProvider from 'next-auth/providers/google'
+// import EmailProvider from 'next-auth/providers/email'
 import GitHubProvider from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import connectDb from '@/db/connectDB';
 import User from '@/models/User';
 import Payment from '@/models/Payment';
 
 
-
-
-export const authOptions = NextAuth({
+export const authoptions = NextAuth({
     providers: [
+        // OAuth authentication providers...
         GitHubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET
         }),
-
-        CredentialsProvider({
-            name: "Domain Account",
-            async authorize(credentials, req) {
-                const user = {
-                    /* add function to get user */
-                }
-                return user
-            },
-            credentials: {
-                // domain: {
-                //     label: "Domain",
-                //     type: "text ",
-                //     placeholder: "CORPNET",
-                //     value: "CORPNET",
-                // },
-                username: { label: "Username", type: "text ", placeholder: "jsmith" },
-                password: { label: "Password", type: "password" },
-            },
-        }),
+        //   AppleProvider({
+        //     clientId: process.env.APPLE_ID,
+        //     clientSecret: process.env.APPLE_SECRET
+        //   }),
+        //   FacebookProvider({
+        //     clientId: process.env.FACEBOOK_ID,
+        //     clientSecret: process.env.FACEBOOK_SECRET
+        //   }),
+        //   GoogleProvider({
+        //     clientId: process.env.GOOGLE_ID,
+        //     clientSecret: process.env.GOOGLE_SECRET
+        //   }),
+        //   // Passwordless / email sign in
+        //   EmailProvider({
+        //     server: process.env.MAIL_SERVER,
+        //     from: 'NextAuth.js <no-reply@example.com>'
+        //   }),
     ],
     callbacks: {
         async signIn({ user, account, profile, email, credentials }) {
-            if (account.provider === "github") {
+            if (account.provider == "github") {
+                await connectDb()
                 const client = await mongoose.connect("mongodb://localhost:27017/javajuice")
-                const currentUser = User.findOne({ email: email })
+                const currentUser = await User.findOne({ email: email })
                 if (!currentUser) {
-                    const newUser = new User({
-                        email: email,
-                        username: email.split("@")[0],
+                    // Create a new user
+                    const newUser = await User.create({
+                        email: user.email,
+                        username: user.email.split("@")[0],
                     })
-                    await newUser.save()
-                    user.name = newUser.username
                 }
-                else{
-                    user.name = currentUser.username
-                }
+                return true
             }
-            return true;
-        }
+        },
+
+        async session({ session, user, token }) {
+            const dbUser = await User.findOne({ email: session.user.email })
+            console.log(dbUser)
+            session.user.name = dbUser.username
+            return session
+        },
     }
 })
 
-export { authOptions as GET, authOptions as POST };
+export { authoptions as GET, authoptions as POST }
