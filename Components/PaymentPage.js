93458d747d8357ab1dbb9164b5756dbecbf731d4
aditehh <aditya.aryal888@gmail.com>
2025-07-22 19:06:34 +0545

    backtothestart

--- Components/PaymentPage.js
+++ Components/PaymentPage.js
@@ -3,30 +3,11 @@ import React, { useEffect } from 'react'
 import Link from 'next/link'
 import CryptoJS from 'crypto-js';
 import { v4 as uuidv4 } from 'uuid';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 
 const PaymentPage = (params) => {
 
-    const [signature, setsignature] = useState("")
-
-    const [amount, setamount] = useState(0)
-
-    useEffect(() => {
-        const msg = `total_amount=${amount},transaction_uuid=${transaction_uuid},product_code=EPAYTEST`;
-        const hash = CryptoJS.HmacSHA256(msg, "8gBm/:&EnhH.1/q");
-        const sign = CryptoJS.enc.Base64.stringify(hash);
-        setsignature(sign)
-    }, [amount, transaction_uuid])
-
-
-    // const total_amount = 2
-    const productCode = "EPAYTEST";
-    const transaction_uuid = uuidv4();
-    // const message = `total_amount=${total_amount},transaction_uuid=${transaction_uuid},product_code=EPAYTEST`
-
-    // var hash = CryptoJS.HmacSHA256(message, "8gBm/:&EnhH.1/q");
-    // var hashInBase64 = CryptoJS.enc.Base64.stringify(hash);
 
     return (
         <>
@@ -69,48 +50,25 @@ const PaymentPage = (params) => {
                     <div className="payments bg-slate-600 p-4 rounded-lg w-1/2">
                         <h2 className="text-white font-bold mb-2">Payments</h2>
                         <div className='flex gap-2 flex-col'>
-                            <form action="https://rc-epay.esewa.com.np/api/epay/main/v2/form" method='POST'>
-                                <div className='flex flex-col gap-2 mt-5'>
-                                    <div>
-                                        <input type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter the Name' />
-                                    </div>
-                                    <div>
-                                        <input type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter the Message' />
-                                    </div>
-                                    <div>
-                                        <input type="text" name="total_amount" id='"total_amount' value={total_amount} className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter the Amount' />
-                                    </div>
+                            <div className='flex flex-col gap-2 mt-5'>
+                                <div>
+                                    <input type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter the Name' />
                                 </div>
-                                <div className='flex gap-2 mt-5'>
-                                    <input type="hidden" id="signature" name="signature" value={hashInBase64} required />
-                                    <input type="hidden" id="signed_field_names" name="signed_field_names" value="total_amount,transaction_uuid,product_code" required />
-                                    <input type="hidden" id="transaction_uuid" name="transaction_uuid" value={transaction_uuid} required />
-                                    <input type="hidden" id="product_code" name="product_code" value="EPAYTEST" required />
-                                    <button type='submit' className='bg-slate-800 p-3 rounded-lg'>$20</button>
-                                    <input type="hidden" id="amount" name="amount" value={total_amount} required />
-                                    <input type="hidden" id="tax_amount" name="tax_amount" value="0" required />
-                                    <input type="hidden" id="product_service_charge" name="product_service_charge" value="0" required />
-                                    <input type="hidden" id="product_delivery_charge" name="product_delivery_charge" value="0" required />
-                                    <input type="hidden" id="success_url" name="success_url" value="https://developer.esewa.com.np/success" required />
-                                    <input type="hidden" id="failure_url" name="failure_url" value="https://developer.esewa.com.np/failure" required />
-                                    <button onClick={() => setamount(30)} className='bg-slate-800 p-3 rounded-lg'>$30</button>
-                                    <button className='bg-slate-800 p-3 rounded-lg'>$99</button>
-
-
-
-
+                                <div>
+                                    <input type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter the Message' />
                                 </div>
-                            </form>
+                                <div>
+                                    <input type="text" name="total_amount" id='"total_amount' className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter the Amount' />
+                                </div>
+                            </div>
+                            <div className='flex gap-2 mt-5'>
+                               <button type="submit" className='bg-slate-800 p-3 rounded-lg'>$20</button>
+                                <button type="submit" className='bg-slate-800 p-3 rounded-lg'>$30</button>
+                                <button type="submit" className='bg-slate-800 p-3 rounded-lg'>$99</button>
+                            </div>
                             <button type='submit' className='bg-slate-800 p-3 rounded-lg w-full'>Pay</button>
-
                         </div>
-
-
                     </div>
-
-
-
-
                 </div>
             </div >
 
